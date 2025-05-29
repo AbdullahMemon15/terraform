feb0b81e58e3e436f9cd09f792ce5c3cb035f7b3
Abdullah <abdullahakhund@gmail.com>
2025-05-29 05:42:47 -0700

    github respository creation using terraform

---
+++ github.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "~> 6.0"
+    }
+  }
+}
+
+# Configure the GitHub Provider
+provider "github" {
+    token = "access key"
+    
+}
+
+resource "github_repository" "example" {
+  name        = "example"
+  description = "My awesome codebase"
+
+  visibility = "public"
+
+  template {
+    owner                = "github"
+    repository           = "terraform-template-module"
+    include_all_branches = true
+  }
+}
+
+
+
