9c8ae2842f7117eefcb63c06b25340a4ac676ab5
Abdullah <abdullahakhund@gmail.com>
2025-05-30 19:30:06 -0700

    Terraform Variables
    Terraform Variables

---
+++ variables.tf
@@ -0,0 +1,16 @@
+variable "vpn_ip" {
+  default = "200.20.30.50/32"
+  description = "this is a vpn server created in aws"
+} 
+
+variable "app_port" {
+  default = "8080"
+} 
+
+variable "ssh_port" {
+  default = "22"
+} 
+
+variable "ftp_port" {
+  default = "21"
+} 
