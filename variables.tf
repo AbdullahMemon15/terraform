7c95826e7fb941c19e40d0e351c5e805115204cd
Abdullah <abdullahakhund@gmail.com>
2025-05-30 19:51:51 -0700

    tfvars usage

--- variables.tf
+++ variables.tf
@@ -1,16 +1,18 @@
-variable "vpn_ip" {
-  default = "200.20.30.50/32"
-  description = "this is a vpn server created in aws"
-} 
+# variable "vpn_ip" {
+#   default = "200.20.30.50/32"
+#   description = "this is a vpn server created in aws"
+# } 
 
-variable "app_port" {
-  default = "8080"
-} 
+# variable "app_port" {
+#   default = "8080"
+# } 
 
-variable "ssh_port" {
-  default = "22"
-} 
+# variable "ssh_port" {
+#   default = "22"
+# } 
 
-variable "ftp_port" {
-  default = "21"
-} 
+# variable "ftp_port" {
+#   default = "21"
+# } 
+
+variable "ami" {description = "this ami is for us-east-1 region"}
