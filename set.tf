e6b6315e32a7051aaab688e89b201bb389124aca
Abdullah <abdullahakhund@gmail.com>
2025-06-08 18:56:12 -0700

    for-each meta argument

---
+++ set.tf
@@ -0,0 +1,16 @@
+# variable "my-list" {
+#   type = list
+#   default =  ["hello", "world","hello"]
+# }
+
+# output "my-list" {
+#   value = var.my-list[0]
+# }
+
+# variable "my-set" {
+#   type = set(string)
+# }
+
+# output "my-set" {
+#   value = var.my-set
+# }
