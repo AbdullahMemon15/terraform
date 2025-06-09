19cedfd81f3dd3d04c15fa846ba2c3e2b0e10bf7
Abdullah <abdullahakhund@gmail.com>
2025-06-08 22:55:01 -0700

    modules in terraform

---
+++ module.tf
@@ -0,0 +1,6 @@
+# module "eks" {
+#   source  = "terraform-aws-modules/eks/aws"
+#   version = "20.11.1"
+#   subnet_ids = ["subnet-021e7b87db88e184a","subnet-039fe8d9eeb59eb60"]
+#   cluster_name = "test"
+# }
