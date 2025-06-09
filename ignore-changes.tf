5a3cbb055d73ed8e78e9ebbefaa5fa0276501acc
Abdullah <abdullahakhund@gmail.com>
2025-06-08 18:23:37 -0700

    implicit dependency

--- ignore-changes.tf
+++ ignore-changes.tf
@@ -1,17 +1,17 @@
-provider "aws" {
-  region     = "us-east-1"
-}
+# provider "aws" {
+#   region     = "us-east-1"
+# }
 
-resource "aws_instance" "myec2" {
-    ami = "ami-0f34c5ae932e6f0e4"
-    instance_type = "t2.micro"
+# resource "aws_instance" "myec2" {
+#     ami = "ami-0f34c5ae932e6f0e4"
+#     instance_type = "t2.micro"
 
-    tags = {
-        Name = "HelloWorld"
-    }
+#     tags = {
+#         Name = "HelloWorld"
+#     }
 
-    lifecycle {
-      ignore_changes = [tags,instance_type]
-    }
+#     lifecycle {
+#       ignore_changes = [tags,instance_type]
+#     }
 
-}
+# }
