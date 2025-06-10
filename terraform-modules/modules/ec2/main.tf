232135e18d87e099422ff81ff346df7b02f6c1c8
Abdullah <abdullahakhund@gmail.com>
2025-06-09 20:06:03 -0700

    module outputs

--- terraform-modules/modules/ec2/main.tf
+++ terraform-modules/modules/ec2/main.tf
@@ -1,21 +1,30 @@
-terraform {
-  required_providers {
-    aws = {
-        source = "hashicorp/aws"
-        version = ">= 5.0"
-    }
-  }
-}
+# terraform {
+#   required_providers {
+#     aws = {
+#         source = "hashicorp/aws"
+#         version = ">= 5.0"
+#     }
+#   }
+# }
+
+# resource "aws_instance" "myec2" {
+#     ami = var.ami
+#     instance_type = var.instance_type
+# }
+    
+#     variable "ami" {
+      
+#     }
+
+#     variable "instance_type" {
+      
+#     }
 
 resource "aws_instance" "myec2" {
-    ami = var.ami
-    instance_type = var.instance_type
+  ami = "ami-08aod1e16fc3f61ea"
+  instance_type = "t2.micro"
 }
-    
-    variable "ami" {
-      
-    }
 
-    variable "instance_type" {
-      
-    }
+output "instance_id" {
+  value = aws_instance.myec2.id
+}
--- terraform-modules/teams/A/module.tf
+++ terraform-modules/teams/A/module.tf
@@ -1,9 +1,14 @@
 provider "aws" {
-  region = "ap-south-1"
+  region = "us-east-1"
 }
 module "ec2" {
   source = "../../modules/ec2"
-  instance_type = "t2.micro"
-  ami = "ami-123"
+#   instance_type = "t2.micro"
+#   ami = "ami-123"
 
+}
+
+resource "aws_eip" "this" {
+  domain = "vpc"
+  instance = module.ec2.instance_id
 }
