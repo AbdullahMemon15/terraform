165eb31f563b55f1253b71c3623ab42008748102
Abdullah <abdullahakhund@gmail.com>
2025-06-11 18:32:59 -0700

    cli driven wrokflow in HCP

---
+++ cli-worklfow.tf
@@ -0,0 +1,16 @@
+# terraform { 
+#   cloud { 
+    
+#     organization = "terraform-practise-003" 
+
+#     workspaces { 
+#       name = "cli-driven-workflow" 
+#     } 
+#   } 
+# }
+
+
+
+# resource "time_sleep" "wait" {
+#   create_duration = "10s"
+# }
